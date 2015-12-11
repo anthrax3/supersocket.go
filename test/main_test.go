96bb3f0c3caf1106aee85ae7e89581ee81120cb0
Kerry Jiang <kerry-jiang@hotmail.com>
2015-12-12 00:55:09 +0800

    first commit

---
+++ test/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func sum(n ...int) int {
+	var c int
+	for _, i := range n {
+		c += i
+	}
+    return c
+}
+
+func TestSum(t *testing.T) {
+	time.Sleep(time.Second * 2)
+
+	if sum(1, 2, 3) != 6 {
+		t.Fatal("sum error!")
+	}
+}
